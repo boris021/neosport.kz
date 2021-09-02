baa9ba4b2d769b95e2864dddc31eede9fc21ecb8
boris <boris021@mail.ru>
2021-09-02 22:35:31 +0500

    sen form

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -77,6 +77,25 @@ if (document.querySelector('.sertificat__slider')) {
 		navigation: {
 			nextEl: '.sertificat__slider .swiper-button_next',
 			prevEl: '.sertificat__slider .swiper-button-prev',
-		}
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 0,
+				autoHeight: true,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			1268: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+		},
 	});
 }
